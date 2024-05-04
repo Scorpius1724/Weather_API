6f4fc01d1ef885e7ab43e95f6b6b70376184bad5
Swapnil <swapnilacharjee1724@gmail.com>
2024-05-05 03:43:12 +0530

    Day 29 - create weather api root page and api page

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template
 
 app = Flask("website")
 
-@app.route("/home/")
+@app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 16
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 app.run(debug=True)
